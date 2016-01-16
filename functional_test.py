37cd4abd3e0594bbdb917b9713288f5ccd685624
liuxinhua <liuxinhua@lmac.lan>
2016-01-16 10:03:19 +0800

    进一步修改

--- functional_test.py
+++ functional_test.py
@@ -43,19 +43,35 @@ class NewVisitorTest(unittest.TestCase):
         #待办事项表格中显示了"1:Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
 
+        # import time
+        # time.sleep(10)
+
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),'没有找到任何可显示的条目'
-        )
+        # self.assertTrue(
+        #     any(row.text == '1:Buy peacock feathers' for row in rows),'没有找到任何可显示的条目'
+        #     '--its text was:\n%s' % (table.text,)
+        # )
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
 
         #页面中又显示了一个文本框,可以输入其它的待办事项
         #她输入了"Use peacock feathers to make a fly"
-
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(u'Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
 
         #页面再次更新,她的清单中显示了这两个待办事项
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2:Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+
 
         #伊丽丝想知道这个网站是否会记住她的清单
 
@@ -67,6 +83,7 @@ class NewVisitorTest(unittest.TestCase):
 
         #她很满意,去睡觉了
 
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 # home_page = None
 
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request,'home.html')
+    return render(request,'home.html',{
+        'new_item_text':request.POST.get('item_text')
+    })
