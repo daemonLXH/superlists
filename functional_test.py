a934b1b87fc5df90063fb4595c6321c91a5b37f5
liuxinhua <liuxinhua@lmac.lan>
2016-01-16 13:55:18 +0800

    重构页面内容测试方法

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         #伊丽丝听说有一个在线待办事项应用
@@ -42,18 +47,19 @@ class NewVisitorTest(unittest.TestCase):
 
         #待办事项表格中显示了"1:Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         # import time
         # time.sleep(10)
 
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
         # self.assertTrue(
         #     any(row.text == '1:Buy peacock feathers' for row in rows),'没有找到任何可显示的条目'
         #     '--its text was:\n%s' % (table.text,)
         # )
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
 
         #页面中又显示了一个文本框,可以输入其它的待办事项
         #她输入了"Use peacock feathers to make a fly"
@@ -64,13 +70,15 @@ class NewVisitorTest(unittest.TestCase):
 
         #页面再次更新,她的清单中显示了这两个待办事项
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        # self.assertIn(
+        #     '2:Use peacock feathers to make a fly',
+        #     [row.text for row in rows]
+        # )
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
 
         #伊丽丝想知道这个网站是否会记住她的清单
