243fe20b8cc197d285c8788be9b56d15395b4107
liuxinhua <liuxinhua@lmac.lan>
2016-01-17 07:40:21 +0800

    为每个用户创建唯一URL 第一步 先规划 功能测试

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -47,10 +47,12 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
 
 
-        #她按回车键后,页面更新了
+        #她按回车键后,被带到了一个新的url
 
         #待办事项表格中显示了"1:Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         # import time
@@ -83,6 +85,36 @@ class NewVisitorTest(LiveServerTestCase):
         # )
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+
+        #现在另一个新用户访问了网站
+
+        ##我们使用一个新浏览器会话
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #他访问首页
+        #页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+
+
+        #他输入一个新待办事项,新建一个清单
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #他获取了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        #这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertIn('Buy milk',page_text)
 
 
         #伊丽丝想知道这个网站是否会记住她的清单
