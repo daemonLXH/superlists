4c27b88c0055da377b6d9509074d8b35a858184d
liuxinhua <liuxinhua@lmac.lan>
2016-01-17 17:33:29 +0800

    添加新URL,新视图函数及新的模板文件list

--- lists/views.py
+++ lists/views.py
@@ -10,7 +10,7 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     # else:
     #     new_item_text = ''
     # item = Item()
@@ -20,5 +20,10 @@ def home_page(request):
     # return render(request,'home.html',{
     #     'new_item_text':new_item_text
     # })
+    # items = Item.objects.all()
+    return render(request,'home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request,'home.html',{'items':items})
+    return render(request,'list.html',{'items':items})
