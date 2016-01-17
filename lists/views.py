386bafef4758b3b162b47fd7f8fd52c0e177749f
liuxinhua <liuxinhua@lmac.lan>
2016-01-17 18:11:12 +0800

    重构视图函数

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,10 @@ from django.http import HttpResponse
 # home_page = None
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     new_item_text = request.POST['item_text']
+    #     Item.objects.create(text=new_item_text)
+    #     return redirect('/lists/the-only-list-in-the-world/')
     # else:
     #     new_item_text = ''
     # item = Item()
@@ -27,3 +27,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html',{'items':items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
