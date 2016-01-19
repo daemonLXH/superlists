1d9eaa8a2e9bb44b66ef3607a2bc8009a6a672c3
liuxinhua <liuxinhua@lmac.lan>
2016-01-20 07:24:30 +0800

    拆分url

--- lists/urls.py
+++ lists/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url,patterns,include
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^$','lists.views.home_page',name='home'),
-    # url(r'^lists/(\d+)/$','lists.views.view_list',name='view_list'),
-    # url(r'^lists/new$','lists.views.new_list',name='new_list'),
-    # url(r'^lists/(\d+)/add_item$','lists.views.add_item',name='add_item'),
-    url(r'^lists/',include('lists.urls')),
+    # url(r'^$','lists.views.home_page',name='home'),
+    url(r'^(\d+)/$','lists.views.view_list',name='view_list'),
+    url(r'^new$','lists.views.new_list',name='new_list'),
+    url(r'^(\d+)/add_item$','lists.views.add_item',name='add_item'),
 ]
